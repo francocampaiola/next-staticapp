a35cc69ab33fb5d1903fa768aea16bbc0b908fcc
Franco Campaiola <francocampaiola@hotmail.com>
2022-09-21 17:50:27 -0300

    ⬆️ Se agrega el archivo _document.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next'
+import { Button } from '@nextui-org/react'
 
 const Home: NextPage = () => {
   return (
     <>
-      <h1>Hello World</h1>
+      <Button color={'gradient'}>Hello world!</Button>
     </>
   )
 }
