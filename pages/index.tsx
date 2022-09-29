241315827a4ed305bbac8192fc4a110968b016f8
Franco Campaiola <francocampaiola@hotmail.com>
2022-09-29 16:11:12 -0300

    :+1: Se incorpora getStaticProps al proyecto

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,24 @@
-import type { NextPage } from 'next'
-import { Button } from '@nextui-org/react'
-import { Layout } from '../components/layouts/Layout'
+import type { GetStaticProps, NextPage } from "next";
+import { Button } from "@nextui-org/react";
+import { Layout } from "../components/layouts/Layout";
 
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
+  const title = "Listado de Pokemons";
 
-  const title = 'Listado de Pokemons'
+  console.log(props);
 
   return (
     <Layout title={title}>
-      <Button color={'gradient'}>Hello world!</Button>
+      <Button color={"gradient"}>Hello world!</Button>
     </Layout>
-  )
-}
+  );
+};
 
-export default Home
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  console.log("Test");
+  return {
+    props: {},
+  };
+};
+
+export default Home;
