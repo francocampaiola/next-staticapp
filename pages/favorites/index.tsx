54c199ced8b381e4ad2c1e3901392303575e3d00
Franco Campaiola <francocampaiola@hotmail.com>
2022-10-06 10:08:42 -0300

    Permite agregar Pokemons a Favoritos

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,11 +1,50 @@
+import { Card, Grid } from "@nextui-org/react"
 import { NextPage } from "next"
+import Link from "next/link"
+import { useEffect, useState } from "react"
 import { Layout } from "../../components/layouts"
-import { NoFavorites } from "../../components/ui"
+import { NoFavorites } from "../../components/ui"
+import localFavorites from "../../utils/localFavorites"
 
 const Favorites: NextPage = () => {
+
+  const [favoritePokemons, setFavoritePokemons] = useState<number[]>([]);
+
+  useEffect(() => {
+    setFavoritePokemons(localFavorites.pokemons())
+  }, []);
+
+
   return (
     <Layout title="Favoritos">
-        <NoFavorites />
+      {favoritePokemons.length === 0 ?
+        (
+          <NoFavorites />
+        )
+        :
+        (
+          <>
+            <Grid.Container>
+              {
+                favoritePokemons.map(id => (
+                  <Grid xs={6} sm={3} md={2} xl={1} key={id} css={{ marginTop: '5px', marginLeft: '10px' }}>
+                    <Card isHoverable isPressable>
+                      <Link href={`/pokemon/${id}`}>
+                        <Card.Image
+                          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
+                          height={300}
+                          width={300}
+                          alt={id.toString()}
+                        />
+                      </Link>
+                    </Card>
+                  </Grid>
+                ))
+              }
+            </Grid.Container>
+          </>
+        )
+      }
     </Layout>
   )
 }
--- utils/localFavorites.ts
+++ utils/localFavorites.ts
@@ -19,7 +19,12 @@ const existInFavorites = (id: number) : boolean => {
     return favorites.includes(id);
 }
 
+const pokemons = () : number[] => {
+    return JSON.parse(localStorage.getItem('favorites') || '[]');
+}
+
 export default {
     toggleFavorite,
-    existInFavorites
+    existInFavorites,
+    pokemons
 }
